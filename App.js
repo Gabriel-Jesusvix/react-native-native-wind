96bb7f53b416fd319a4a192e5c52b86b4174afff
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2022-11-30 00:09:30 -0300

    config nativeWind and first steep from use

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-lg"> Login NativeWind!</Text>
+        <TextInput
+          className="w-[90%] px-4 m-2 rounded-md h-11 bg-slate-300 "
+          value={name}
+          onChangeText={(name) => {
+            setName(name);
+          }}
+          placeholder="Seu nome"
+        />
+        <TextInput
+          className="w-[90%] px-4 m-2 rounded-md h-11 bg-slate-300 "
+          value={password}
+          onChangeText={(password) => {
+            setPassword(password);
+          }}
+          secureTextEntry
+          placeholder="Digite sua senha..."
+        />
+        <TouchableOpacity
+          title="Login"
+          className="w-[90%] items-center justify-center h-10 mt-5 rounded-md bg-lime-300"
+          onPress={() => alert("Login")}
+        >
+          <Text className="text-lg text-gray-900">Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
